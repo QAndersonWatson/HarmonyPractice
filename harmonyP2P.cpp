2bf65923c0d0aefb8032868cbcea7bb3d3d1f04b
QAndersonWatson <93205729+QAndersonWatson@users.noreply.github.com>
2025-01-30 11:18:22 -0500

    Update harmonyP2P.cpp

--- harmonyP2P.cpp
+++ harmonyP2P.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sstream>
+#include "research_interface.h"
+
+#define PORT 12346
+#define BUFFER_SIZE 2048
+#define HARMONY_IP "192.168.2.1"
 
